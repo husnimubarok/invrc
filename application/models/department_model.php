36a7785c896a83504e23496af0aa462b4cad6b15
farisi <salmandriva@gmail.com>
2015-05-31 16:26:31 +0700

    membuat department model

---
+++ application/models/department_model.php
@@ -0,0 +1,14 @@
+<?php
+
+/* GPL
+ * GPL To change this license header, choose License Headers in Project Properties.
+ * GPL To change this template file, choose Tools | Templates
+ * GPL and open the template in the editor.
+ */ GPL
+/*
+ * name        : Salman Farisi<salmandriva@gmail.com>
+ * skype       : salmandriva
+ * YM          : s4lm4ndrake
+
+ */
+?>
