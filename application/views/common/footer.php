989dcfdbe27429c872a024f876267248b8d806a1
farisi <salmandriva@gmail.com>
2015-05-31 16:27:05 +0700

    menambah link  script file bootstrap-datepicker.js

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -6,6 +6,8 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js');?>"></script>
-    <script src="<?php echo base_url('bootstrap/js/docs.min.js');?>"></script>
+    <!--<script src="<?php //echo base_url('bootstrap/js/docs.min.js');?>"></script> -->
+    <script src="<?php echo base_url('bootstrap/js/bootstrap-datepicker.js');?>"></script>
+    <script
   </body>
 </html>
